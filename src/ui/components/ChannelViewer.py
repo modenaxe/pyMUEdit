3047941e2aa91787db38e61b86c960294de8a13f
Sara Papandrea <papandrea.sara@outlook.com>
2025-06-28 10:12:45 +1000

    added different channel colours and fixed axis alignments

--- src/ui/components/ChannelViewer.py
+++ src/ui/components/ChannelViewer.py
@@ -17,7 +17,8 @@ class ChannelViewer(QWidget):
         self.layout.addWidget(self.canvas)
         self.setLayout(self.layout)
 
-        self.update_plot()  # Display initial plot
+        # display initial plot
+        self.update_plot()
 
     def set_channel_range(self, indices):
         self.channel_indices = indices
@@ -25,18 +26,23 @@ class ChannelViewer(QWidget):
 
     def update_plot(self):
         self.figure.clear()
+        colours = ["red", "sienna", "olive", "limegreen", "lightseagreen", "royalblue", "blueviolet", "mediumorchid"]
 
         # create one subplot for each channel in the index range
         n = len(self.channel_indices)
         for i, index in enumerate(self.channel_indices):
             ax = self.figure.add_subplot(n, 1, i + 1)
-            ax.plot(self.entire_emg_data[index], linewidth=0.8)
-            ax.set_ylabel(f"Ch {index + 1}")
+            ax.plot(self.entire_emg_data[index], linewidth=0.8, color=colours[i])
+            ax.set_ylabel(f"Ch {index + 1}", fontsize=14, labelpad=15)
             ax.grid(True)
+            ax.set_yticklabels([])
+            # hide x-axis label (except for last plot)
             if i < n - 1:
-                # hide x-axis label (except for the last one)
                 ax.set_xticklabels([])
 
-        ax.set_title(f"Channels {self.channel_indices[0]}-{self.channel_indices[7]}")
-        ax.set_xlabel("Time")
+            # add title for first plot only
+            if i == 0:
+                ax.set_title(f"Channels {self.channel_indices[0]}-{self.channel_indices[7]}", fontsize=20, pad=15)
+
+        ax.set_xlabel("Time", fontsize=20, labelpad=15)
         self.canvas.draw()
