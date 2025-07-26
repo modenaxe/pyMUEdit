73d768041b202b7b7599b775e4e020e9b1019aab
Liam Murphy <liampm32@gmail.com>
2025-07-26 11:11:24 +1000

    Test and fix get_spikes (#12)
    * Test and fix get_spikes
    
    I can't get either sklearn or scipy's kmeans implementation to match
    MATLAB's exactly, so I opted to just test that they agree on most (95%)
    of the spikes instead.
    
    * Add missing gen_inputs.m

--- tests/gen_inputs.m
+++ tests/gen_inputs.m
@@ -3,3 +3,19 @@ save('ExpOut20OpentOTBplus.mat')
 
 demsignals = demean(signal.data);
 save('ExpOut20Demean.mat')
+
+[E, D] = pcaesig(signal.data);
+[whitensignals, whiteningMatrix, dewhiteningMatrix] = whiteesig(signal.data, E, D);
+save('ExpOut20Whiteesig.mat');
+
+% Act like this is the first iteration.
+X = whitensignals;
+w = randn(size(whitensignals, 1), 1);
+B = zeros(size(whitensignals, 1), 75);
+w_skew = fixedpointalg(w, X, B, 500, 'skew');
+w_kurtosis = fixedpointalg(w, X, B, 500, 'kurtosis');
+w_logcosh = fixedpointalg(w, X, B, 500, 'logcosh');
+save('ExpOut20FixedPointAlg.mat');
+
+[icasig, spikes2] = getspikes(w_skew, X, signal.fsamp);
+save('ExpOut20GetSpikes.mat');
