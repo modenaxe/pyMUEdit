1421f2557cb050fab01bbed9a8c0897cfb327c95
z5513332 <alex1998122@outlook>
2025-07-26 18:44:26 +0800

    Update main.py and add scaling_config.py for screen scaling support

---
+++ src/core/utils/scaling_config.py
@@ -0,0 +1,51 @@
+# core/utils/scaling_config.py
+
+import os
+import sys
+from PyQt5.QtCore import Qt, QCoreApplication
+from PyQt5.QtGui import QGuiApplication
+
+def get_adaptive_scale():
+    screen = QGuiApplication.primaryScreen()
+
+    # geometry = logical size
+    geometry = screen.geometry()
+    logical_width = geometry.width()
+    logical_height = geometry.height()
+
+    # devicePixelRatio = scale factor from system (e.g. 1.5 for 150%)
+    scale_factor = screen.devicePixelRatio()
+
+    # approximate physical resolution
+    physical_width = int(logical_width * scale_factor)
+    physical_height = int(logical_height * scale_factor)
+
+    # use physical resolution to decide scaling
+    if physical_width >= 2560:
+        return "1.5"
+    elif physical_width >= 1920:
+        return "1.25"
+    else:
+        return "1.0"
+
+
+def apply_qt_scaling():
+    """
+    Apply high DPI scaling and fixed Qt screen scale factor
+    based on screen resolution.
+    """
+    # Enable high DPI support before QApplication is created
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+    # Temporary QApplication just to access screen info
+    temp_app = QGuiApplication(sys.argv)
+    scale = get_adaptive_scale()
+    temp_app.quit()
+
+    # Apply fixed scaling
+    # os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = "1"
+    os.environ["QT_SCREEN_SCALE_FACTORS"] = scale
+
+    print(f"[INFO] Adaptive QT scale factor applied: {scale}")
--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ sys.path.insert(0, project_root)
 
 from app.HDEMGDashboard import HDEMGDashboard
 from PyQt5.QtWidgets import QApplication
+from core.utils.scaling_config import apply_qt_scaling # Added for high DPI scaling attributes
+
 
 import warnings
 warnings.filterwarnings("ignore") #ignore warning
@@ -15,9 +17,12 @@ def main():
     """
     Main function to launch the HDEMG Dashboard application.
     """
+    # Configure DPI scaling settings to prevent UI distortion under high-resolution displays || Modified by alex
+    apply_qt_scaling()
+    
     # Create the application
     app = QApplication(sys.argv)
-
+    
     # Set application properties
     app.setApplicationName("HDEMG Analysis Tool")
     app.setOrganizationName("EMG Lab")
