95a528d9bc7b0b830d92fe3e274129f19c71f070
Sara Papandrea <papandrea.sara@outlook.com>
2025-07-29 16:36:22 +1000

    created the ui for lamps (with colour change for selection/deselection)

--- src/ui/components/ConfigurationPanel.py
+++ src/ui/components/ConfigurationPanel.py
@@ -1,5 +1,8 @@
+import os
+
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSizePolicy, QCheckBox, QScrollArea
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap, QColor, QPainter, QPen
+from PyQt5.QtCore import Qt, QPoint, QRect
 
 from ui.components.ActionButton import ActionButton
 from ui.components.CleanCard import CleanCard
@@ -8,11 +11,14 @@ from ui.components.CleanTheme import CleanTheme
 from ui.components.CollapsiblePanel import CollapsiblePanel
 from ui.components.FormDropdown import FormDropdown
 from ui.components.FormInput import FormInput
+from ui.components.FormSpinBox import FormSpinBox
 
 class InputPanel(CollapsiblePanel):
-    def __init__(self, title, gridname, musclename, parent=None):
-        checkbox = QCheckBox()
-        super().__init__(title, checkbox=checkbox, parent=parent)
+    def __init__(self, title, gridname, musclename, checkbox_callback, parent=None):
+        self.checkbox = QCheckBox()
+        # connect the state change of the checkbox to the lamp colour change functionality
+        self.checkbox.stateChanged.connect(lambda state: checkbox_callback(title, state))
+        super().__init__(title, checkbox=self.checkbox, parent=parent)
 
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
 
@@ -25,13 +31,48 @@ class InputPanel(CollapsiblePanel):
         self.muscle_input.input.setText(musclename)
         self.add_widget(self.muscle_input)
 
+class QuattrocentoVisualisation(QLabel):
+    def __init__(self, image_path, parent=None):
+        super().__init__(parent)
+        self.pixmap = QPixmap(image_path)
+        self.setPixmap(self.pixmap)
+        self.setFixedSize(self.pixmap.size())
+        self.setScaledContents(True)
+
+        self.lamps = {}
+
+    def add_lamp(self, name, rect, colour):
+        # Add a new lamp box to the quattrocento image
+        self.lamps[name] = [rect, colour]
+
+    def update_lamp_colour(self, name, new_colour):
+        # Change the colour of the specific lamp box (by index)
+        if name in self.lamps:
+            self.lamps[name][1] = new_colour
+            self.update()
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        # Create all of the lamp boxes (overlaid on top of quattrocento image)
+        for lamp in self.lamps.values():
+            painter.setBrush(lamp[1])
+            painter.setPen(QPen(lamp[1].darker(150), 2))
+            painter.drawRect(lamp[0])
+
+        painter.end()
+
 class ConfigurationPanel(QWidget):
     def __init__(self, emg_obj, parent=None):
         super().__init__(parent)
 
         self.emg_obj = emg_obj
+        self.data = emg_obj.signal_dict["data"]
 
-        self.setMinimumSize(1024, 600)
+        self.setMinimumSize(1600, 750)
 
         # Setup main layout
         main_layout = QVBoxLayout()
@@ -47,8 +88,8 @@ class ConfigurationPanel(QWidget):
         left_layout.setSpacing(15)
 
         # signal range dropdown panel
-        left_layout.addWidget(InputPanel("Splitter #1", "GR04MM1305", ""))
-        left_layout.addWidget(InputPanel("Splitter #2", "GR04MM1305", ""))
+        left_layout.addWidget(InputPanel("Splitter #1", "GR04MM1305", "", self.checkbox_state_change))
+        left_layout.addWidget(InputPanel("Splitter #2", "GR04MM1305", "", self.checkbox_state_change))
 
         left_layout.addStretch()
 
@@ -59,17 +100,42 @@ class ConfigurationPanel(QWidget):
 
         # middle panel
         middle_container = CleanCard()
+        middle_container.layout.setSpacing(50)
 
-        middle_layout = QVBoxLayout(middle_container)
-        middle_layout.setSpacing(15)
-
-        # Add title
-        page_title = QLabel("Channel Viewer")
-        page_title.setFont(QFont("Segoe UI", 14, QFont.Bold))
+        # add title
+        page_title = QLabel("Quattrocento Visualisation")
+        page_title.setFont(QFont("Segoe UI", 30, QFont.Bold))
         page_title.setStyleSheet(f"color: {CleanTheme.TEXT_PRIMARY};")
-        middle_layout.addWidget(page_title)
+        page_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        middle_container.layout.addWidget(page_title)
 
-        middle_layout.addStretch()
+        # create the quattrocento visualisation
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Quattrocento.jpg")
+
+        self.quattrocento_label = QuattrocentoVisualisation(image_path)
+        # Create lamps for splitters 1 and 2
+        self.quattrocento_label.add_lamp("Splitter #1",
+                                         QRect(33, 110, 440, 80), QColor(255, 0, 0, 100))
+        self.quattrocento_label.add_lamp("Splitter #2",
+                                         QRect(517, 110, 440, 80), QColor(255, 0, 0, 100))
+
+        # Create lamps for mixed inputs 1-4
+        self.quattrocento_label.add_lamp("Multiple Inputs #1",
+                                         QRect(20, 342, 220, 60), QColor(255, 0, 0, 100))
+        self.quattrocento_label.add_lamp("Multiple Inputs #2",
+                                         QRect(257, 342, 220, 60), QColor(255, 0, 0, 100))
+        self.quattrocento_label.add_lamp("Multiple Inputs #3",
+                                         QRect(495, 342, 220, 60), QColor(255, 0, 0, 100))
+        self.quattrocento_label.add_lamp("Multiple Inputs #4",
+                                         QRect(733, 342, 220, 60), QColor(255, 0, 0, 100))
+        middle_container.layout.addWidget(self.quattrocento_label)
+
+        # add number of channels input box
+        num_channels_input_box = FormSpinBox("Number of Channels", 1, 1, 500)
+        num_channels_input_box.spinbox.setValue(self.data.shape[0])
+        middle_container.layout.addWidget(num_channels_input_box)
+
+        middle_container.layout.addStretch()
 
         # right panel
         scroll_area = QScrollArea()
@@ -84,10 +150,14 @@ class ConfigurationPanel(QWidget):
         right_layout.setContentsMargins(0, 0, 0, 0)
         right_layout.setSpacing(15)
 
-        right_layout.addWidget(InputPanel("Multiple Inputs #1", "GR04MM1305", "Tibialis Anterior"))
-        right_layout.addWidget(InputPanel("Multiple Inputs #2", "GR04MM1305", "Tibialis Anterior"))
-        right_layout.addWidget(InputPanel("Multiple Inputs #3", "GR04MM1305", ""))
-        right_layout.addWidget(InputPanel("Multiple Inputs #4", "GR04MM1305", ""))
+        right_layout.addWidget(InputPanel("Multiple Inputs #1", "GR04MM1305", "Tibialis Anterior",
+                                          self.checkbox_state_change))
+        right_layout.addWidget(InputPanel("Multiple Inputs #2", "GR04MM1305", "Tibialis Anterior",
+                                          self.checkbox_state_change))
+        right_layout.addWidget(InputPanel("Multiple Inputs #3", "GR04MM1305", "",
+                                          self.checkbox_state_change))
+        right_layout.addWidget(InputPanel("Multiple Inputs #4", "GR04MM1305", "",
+                                          self.checkbox_state_change))
 
         right_layout.addStretch()
         scroll_area.setWidget(right_container)
@@ -103,6 +173,16 @@ class ConfigurationPanel(QWidget):
 
         self.resize(1024, 600)
 
+    def checkbox_state_change(self, name, state):
+        if state == Qt.Checked:
+            # When checked, lamp will green
+            new_colour = QColor(0, 255, 0, 100)
+        else:
+            # When unchecked, lamp will red
+            new_colour = QColor(255, 0, 0, 100)
+
+        self.quattrocento_label.update_lamp_colour(name, new_colour)
+
     def doneClicked(self):
         # TODO: set configuration in emg_obj
-        return
+        self.close()
