bcae96d36b078c2cda1143a12ea7b16ff9767398
clxyrz <clairefan27@gmail.com>
2025-08-10 17:32:32 +1000

    added results table error cases

--- src/ui/components/muAnalysisComponents/ErrorDialog.py
+++ src/ui/components/muAnalysisComponents/ErrorDialog.py
@@ -3,8 +3,7 @@ from PyQt5.QtGui import QColor, QFont
 from PyQt5.QtWidgets import (QDialog, QHBoxLayout, QLabel, QPushButton, QStyle,
                              QVBoxLayout)
 
-from ui.components.muAnalysisComponents.GeneralRedButton import \
-    GeneralRedButton
+from ui.components.muAnalysisComponents.GeneralRedButton import GeneralRedButton
 
 
 class ErrorDialog(QDialog):
