6506469239e2b22f571dcc1a8dde0961ba322df5
Louie Gu <louietlgu@gmail.com>
2025-11-04 02:03:36 +1100

    breakfix: reimplementing plot_idr due to bug in openhdemg

--- src/app/muAnalysisFunctions/FileUploadFunc.py
+++ src/app/muAnalysisFunctions/FileUploadFunc.py
@@ -6,6 +6,12 @@ from ui.components.muAnalysisComponents.ConfirmationDialog import ConfirmationDi
 from ui.components.muAnalysisComponents.SaveablePlot import SaveablePlot
 from ui.components.muAnalysisComponents.ErrorDialog import ErrorDialog
 
+from app.muAnalysisFunctions.CommonOpenFunc import CommonOpenFunc
+
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+
 # from h5py import File as h5py
 
 import openhdemg.library as emg
@@ -89,8 +95,6 @@ class FileUploadFunc:
             self.file_path = file_path
             self.import_data(analysis_plot, FileUploadFunc.file)
 
-        print(self.data_loaded())
-
         return error
 
     def import_data(self, analysis_plot, emgfile):
@@ -98,10 +102,12 @@ class FileUploadFunc:
         Params: filepath, analysis_plot: centre plot instance, emgfile
         Returns: None
         """
+
         if emgfile:
-            fig = emg.plot_idr(FileUploadFunc.file, showimmediately=False)
-            canvas = SaveablePlot(fig)  # plotting in centre with the data now handled
-            analysis_plot.display_fig(canvas)
+            # fig = emg.plot_idr(emgfile, showimmediately=False)
+            # canvas = SaveablePlot(fig)  # plotting in centre with the data now handled
+            # analysis_plot.display_fig(canvas)
+            self.plot_idr(emgfile, analysis_plot)
         else:
             ErrorDialog("Loaded File has errors", "Error").exec_()
 
@@ -152,3 +158,90 @@ class FileUploadFunc:
             error_dialog.setInformativeText("Failed to reload the original file")
             error_dialog.setWindowTitle("Error")
             error_dialog.exec_()
+
+    """
+    needs to be retained due to bug in existing openHDEMG library which
+    errors on plotting subsequent figures after initial import
+    """
+    def plot_idr(
+        self,
+        emgfile,
+        analysis_plot,
+        munumber="all",
+        addrefsig=True,
+        timeinseconds=True,
+        figsize=[20, 15],
+    ):
+        """from openHDEMG to plot idr on graph when first loaded
+        Params (relevant to us): emgfile, analysis_plot(instance of centre plot)
+        Returns: None
+        """
+        common = CommonOpenFunc()
+        idr = common.compute_idr(emgfile=emgfile)
+        if isinstance(munumber, str):
+            if emgfile["NUMBER_OF_MUS"] == 1:
+                munumber = 0
+            else:
+                munumber = [*range(0, emgfile["NUMBER_OF_MUS"])]
+        if isinstance(munumber, list) and len(munumber) == 1:
+            munumber = munumber[0]
+        figname = "aditi_unique_name"
+        plt.close()  # This is to prevent plots from overlaying in centre on repeated uploads
+        fig, ax1 = plt.subplots(
+            figsize=(figsize[0] / 2.54, figsize[1] / 2.54),
+            num=figname,
+        )
+        if isinstance(munumber, int):
+            ax1.plot(
+                idr[munumber]["timesec" if timeinseconds else "mupulses"],
+                idr[munumber]["idr"],
+                ".",
+                markersize=12,
+            )
+            ax1.set_ylabel("MU {} (pps)".format(munumber))
+            ax1.set_xlabel("Time (Sec)" if timeinseconds else "Samples")
+        elif isinstance(munumber, list):
+            idr_all = pd.DataFrame({key: df["idr"] for key, df in idr.items()})
+            idr_all = idr_all[munumber]
+            common = CommonOpenFunc()
+            norm_idr_all = common.min_max_scaling(data=idr_all, col_by_col=False)
+            for count, thisMU in enumerate(munumber):
+                norm_idr = norm_idr_all[thisMU]
+                if norm_idr.mean() <= 0.5:
+                    norm_idr = norm_idr + (0.5 - norm_idr.mean()) + count
+                else:
+                    norm_idr = norm_idr - (norm_idr.mean() - 0.5) + count
+                ax1.plot(
+                    idr[thisMU]["timesec" if timeinseconds else "mupulses"][1:],
+                    norm_idr.dropna(),
+                    ".",
+                    markersize=8,
+                )
+            ax1.set_yticks(np.arange(0.5, len(munumber) + 0.5, 1))
+            ax1.set_yticklabels([str(mu) for mu in munumber])
+            ax1.set_ylabel("Motor units")
+            ax1.set_xlabel("Time (Sec)" if timeinseconds else "Samples")
+        else:
+            raise TypeError(
+                "While calling the plot_idr function, you should pass an "
+                + "integer, a list or 'all' to munumber"
+            )
+        if addrefsig:
+            if not isinstance(emgfile["REF_SIGNAL"], pd.DataFrame):
+                raise TypeError(
+                    "REF_SIGNAL is probably absent or it is not contained in a "
+                    + "dataframe"
+                )
+            x_axis = (
+                emgfile["REF_SIGNAL"].index / emgfile["FSAMP"]
+                if timeinseconds
+                else emgfile["REF_SIGNAL"].index
+            )
+            ax2 = ax1.twinx()
+            ax2.plot(x_axis, emgfile["REF_SIGNAL"][0], color='#555555')
+            ax2.set_ylabel("MVC")
+            ax2.set_zorder(0)
+            ax1.set_zorder(1)
+            ax1.patch.set_alpha(0)
+        canvas = SaveablePlot(fig)  # plotting in centre with the data now handled
+        analysis_plot.display_fig(canvas)
