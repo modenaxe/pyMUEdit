de73c6d30de2b0ffeee02935bff4b2434afd70fd
Brian <brian@student.unsw.edu.au>
2025-11-03 21:59:21 +1100

    Fixed git stash merge from separate branch

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ sys.path.insert(0, project_root)
 from app.ImportDataWindow import ImportDataWindow
 from PyQt5.QtWidgets import QApplication
 from core.utils.config.scaling_config import apply_qt_scaling # Added for high DPI scaling attributes
+from core.database.database import init_db
 
 
 import warnings
