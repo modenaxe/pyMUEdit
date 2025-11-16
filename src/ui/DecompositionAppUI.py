48eea8ccbe62cfd3f9a4ec9eab8179780833560c
Colleen <colleen.lam7@gmail.com>
2025-11-16 20:24:28 +1100

    fix: next button now successfully navigates to editing tab and also loads file in

--- src/ui/DecompositionAppUI.py
+++ src/ui/DecompositionAppUI.py
@@ -59,14 +59,14 @@ def setup_ui(main_window):
 
     main_window.main_layout.addWidget(content_widget)
 
-    # def go_to_import_data():
-    #     main_window.window().show_import_data_view()
+    def go_to_import_data():
+        main_window.window().show_import_data_view()
 
-    # def go_to_editing():
-    #     main_window.window().show_manual_editing_view()
+    def go_to_editing():
+        main_window.open_editing_mode()
     main_window.footer = Footer(
-        # on_prev=go_to_import_data,
-        # on_next=go_to_editing
+        on_prev=go_to_import_data,
+        on_next=go_to_editing
     )
     main_window.footer.setFixedHeight(64)
     main_window.main_layout.addWidget(main_window.footer)
