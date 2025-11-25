6621fb1b002a3ffa63c8b579a06003c71bf4f7ea
Colleen <colleen.lam7@gmail.com>
2025-11-25 21:41:23 +1100

    feat: allow both options to be available after changing reference signal

--- src/ui/components/SegmentSessionPage.py
+++ src/ui/components/SegmentSessionPage.py
@@ -145,10 +145,7 @@ class SegmentSessionPage(QWidget):
 
     def on_reference_signal_change(self):
         self.windows_dropdown.spinbox.setValue(0)
-
-        self.threshold_dropdown.spinbox.blockSignals(True)
         self.threshold_dropdown.spinbox.setValue(0)
-        self.threshold_dropdown.spinbox.blockSignals(False)
 
         self.vis_plot.clear()
         if self.reference_dropdown.dropdown.currentText() == "EMG amplitude":
